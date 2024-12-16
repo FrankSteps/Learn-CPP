707e9d3f6259aa549f02d450d83d1a7158171189
FrankSteps <franciscopassos.contato@gmail.com>
2024-12-16 20:17:42 -0300

    add ternary operator

--- 11_ternaryoperator.cpp
+++ 11_ternaryoperator.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main(){
+    //int grade = 75;
+    //grade  >= 60? cout << "you pass": cout << "you fail";
+
+    int number = 9; 
+    number % 2 == 0;
     return 0;
 }
