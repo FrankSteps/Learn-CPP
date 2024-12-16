675126345a045a07cb9a125120231eb48e5582ac
FrankSteps <franciscopassos.contato@gmail.com>
2024-12-16 20:37:59 -0300

    ternary operator again...

--- 11_ternaryoperator.cpp
+++ 11_ternaryoperator.cpp
@@ -5,7 +5,10 @@ int main(){
     //int grade = 75;
     //grade  >= 60? cout << "you pass": cout << "you fail";
 
-    int number = 9; 
-    number % 2 == 0;
+    //bool hungry = true; 
+    //hungry ? cout << "Go drink water now" : cout << "bruh" ;
+
+    int number = 47; 
+    number % 2 ? cout << "ODD": cout << "EVEN";
     return 0;
 }
