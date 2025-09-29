75d6065fe6a3a0dd27a9df71d9967b7c8cf48867
FrankSteps <franciscopassos.contato@gmail.com>
2025-09-29 00:15:47 -0300

    headers .h -> 17

---
+++ 17_headers/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "math_utils.h"
+
+int main(){
+    int x = 2, y = 5;
+
+    std::cout << "soma: " << soma(x,y) << "\n";
+    std::cout << "multiplicação: " << mult(x,y) << "\n";
+
+    return 0;
+}
---
+++ 17_headers/math_utils.h
@@ -0,0 +1,7 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+int soma(int a, int b);
+int mult(int a, int b);
+
+#endif
